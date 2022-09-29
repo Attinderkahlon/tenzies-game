eec767250ab9de55f0427b4e0c8b87ee8cc8de63
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-09-29 16:12:32 +1000

    layout setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Die from './Die'
+
+function App() {
+  return (
+    <main className='grid-container'>
+      <Die value={1} />
+      <Die value={4} />
+      <Die value={6} />
+      <Die value={3} />
+      <Die value={5} />
+      <Die value={1} />
+      <Die value={4} />
+      <Die value={5} />
+      <Die value={3} />
+      <Die value={2} />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Die = ({ value }) => {
+  return (
+    <>
+      <h1 className='grid-item'>{value}</h1>
+    </>
+  )
+}
+
+export default Die
